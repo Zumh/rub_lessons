4d2ef789a8db6a0a39cb6a59764571f05fb01785
Zumh <17211423+Zumh@users.noreply.github.com>
2025-01-09 08:30:11 -0500

    weight in

--- while.rb
+++ while.rb
@@ -1,13 +1,20 @@
 weight = 89
-num_pallets = 77
+num_pallets = 27
+class Pallet
+  attr_accessor :weight
+end
 def next_pallet()
-    return 78
+  pallet = Pallet.new
+  pallet.weight = 78
+  return pallet
 end
 while weight < 100 and num_pallets <= 30
   pallet = next_pallet()
   weight += pallet.weight
   num_pallets += 1
-
+  
+  puts num_pallets
+  puts "weight is #{weight}"
 end
 
 
