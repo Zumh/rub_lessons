52b7287ce924149af88de48697829c7cb0b73a2b
Zumh <17211423+Zumh@users.noreply.github.com>
2025-01-09 09:29:20 -0500

    if condition added

--- if.rb
+++ if.rb
@@ -1,3 +1,4 @@
+puts "If condition"
 count = 9 
 
 if count > 10
@@ -7,3 +8,14 @@ elseif tries == 3
 else
   puts "Enter a number"
 end
+
+puts "Another If condition\n"
+
+radiation = 8000
+puts "Danger, Will Robinson is greater than #{radiation}" if radiation > 3000
+
+
+distance = 899
+distance = distance * 1.2 while distance < 100
+
+puts "distance #{distance}"
