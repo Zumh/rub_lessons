79cd53b60b24457be1ff671c5693893d9d55e3a7
Zumh <17211423+Zumh@users.noreply.github.com>
2025-01-08 19:07:44 -0500

    lesson 2 methods

---
+++ 2_method.rb
@@ -0,0 +1,9 @@
+def say_goodnight(name)
+    result = 'Good night, ' + name
+    return result
+end
+
+# Time for bed 
+puts say_goodnight('Mary-Ellen') # => 'Goodnight, Mary-Ellen'
+puts say_goodnight('John-Boy') # => 'Goodnight, John-Boy' 
+
